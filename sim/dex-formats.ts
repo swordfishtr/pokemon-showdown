31620eed1e8e54130f8397a1732515c91f2d3c02
demir <demir.ab@protonmail.com>
2025-06-04 17:42:14 +0300

    Debug formats duplicate id

--- sim/dex-formats.ts
+++ sim/dex-formats.ts
@@ -586,6 +586,7 @@ export class DexFormats {
 			if (!Array.isArray(customFormats)) {
 				throw new TypeError(`Exported property 'Formats' from "./config/custom-formats.ts" must be an array`);
 			}
+			console.log(`Custom formats: ${[...customFormats.values()].map(x => x.name ?? x.section ?? '?').join(' | ')}`);
 		} catch (e: any) {
 			if (e.code !== 'MODULE_NOT_FOUND' && e.code !== 'ENOENT') {
 				throw e;
@@ -595,8 +596,8 @@ export class DexFormats {
 		if (!Array.isArray(Formats)) {
 			throw new TypeError(`Exported property 'Formats' from "./config/formats.ts" must be an array`);
 		}
-		if (customFormats) Formats = mergeFormatLists(Formats as any, customFormats);
 		console.log(`Formats: ${[...Formats.values()].map(x => x.name ?? x.section ?? '?').join(' | ')}`);
+		if (customFormats) Formats = mergeFormatLists(Formats as any, customFormats);
 
 		let section = '';
 		let column = 1;

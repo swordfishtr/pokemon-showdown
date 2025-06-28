273b0eb7cd109ca5c15afa0a21e2222c03312334
demir <demir.ab@protonmail.com>
2025-06-28 05:35:49 +0300

    Perfect E1: update roster

--- config/formats.ts
+++ config/formats.ts
@@ -557,12 +557,12 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		desc: `Only 35 Pok&eacute;mon are legal.`,
 		mod: 'gen7',
 		ruleset: ['Standard', '!Sleep Clause Mod', 'Sleep Moves Clause', 'Evasion Abilities Clause'],
-		banlist: ['All Pokemon'],
+		banlist: ['All Pokemon', 'Regenerator'],
 		unbanlist: [
-			'Golurk', 'Flareon', 'Lycanroc-Base', 'Togedemaru-Base', 'Froslass', 'Roserade', 'Magmortar', 'Abomasnow-Base', 'Abomasnow-Mega', 'Audino-Base', 'Audino-Mega', 'Banette-Base', 'Banette-Mega', 'Blastoise-Base', 'Blastoise-Mega', 'Altaria-Base', 'Bronzong', 'Diancie-Base', 'Flygon', 'Forretress', 'Gallade-Base', 'Garbodor', 'Gigalith', 'Golisopod', 'Guzzlord', 'Hitmontop', 'Moltres-Base', 'Milotic', 'Miltank', 'Primeape', 'Rotom-Frost', 'Seismitoad', 'Shaymin-Base', 'Silvally-Fairy', 'Skuntank', 'Swellow', 'Vikavolt-Base', 'Xatu', 'Zoroark-Base',
+			'Golurk', 'Flareon', 'Lycanroc-Base', 'Togedemaru-Base', 'Froslass', 'Roserade', 'Magmortar', 'Abomasnow-Base', 'Abomasnow-Mega', 'Audino-Base', 'Audino-Mega', 'Banette-Base', 'Banette-Mega', 'Blastoise-Base', 'Blastoise-Mega', 'Noivern', 'Bronzong', 'Diancie-Base', 'Flygon', 'Forretress', 'Gallade-Base', 'Garbodor', 'Gigalith', 'Golisopod', 'Guzzlord', 'Hariyama', 'Moltres-Base', 'Milotic', 'Miltank', 'Primeape', 'Rotom-Frost', 'Seismitoad', 'Shaymin-Base', 'Silvally-Fairy', 'Skuntank', 'Swellow', 'Vikavolt-Base', 'Xatu', 'Zoroark-Base',
 		],
 		onValidateSet(set) {
-			const zUsers: string[] = ['golurk', 'flareon', 'lycanroc', 'togedemaru', 'froslass', 'roserade', 'magmortar'];
+			const zUsers: string[] = ['golurk', 'flareon', 'lycanroc', 'primeape', 'rotomfrost', 'roserade', 'magmortar'];
 			const item = this.dex.items.get(set.item);
 			if (item.zMove && !zUsers.includes(toID(set.species))) {
 				return [`${set.name} is not allowed to hold a Z Crystal in E1.`];
@@ -570,7 +570,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		},
 		onBegin() {
 			const megaUsers: string[] = ['blastoise', 'abomasnow', 'audino', 'banette'];
-			const zUsers: string[] = ['golurk', 'flareon', 'lycanroc', 'togedemaru', 'froslass', 'roserade', 'magmortar'];
+			const zUsers: string[] = ['golurk', 'flareon', 'lycanroc', 'primeape', 'rotomfrost', 'roserade', 'magmortar'];
 			this.add('rule', `Perfect E1: Only the following Pokemon can Mega Evolve: ${megaUsers.join(', ')}`);
 			this.add('rule', `Perfect E1: Only the following Pokemon can use Z Moves: ${zUsers.join(', ')}`);
 			for (const pokemon of this.getAllPokemon()) {

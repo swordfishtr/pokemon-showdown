08f5aaaf6f862fb8781c1302e60a46a059d56ded
demir <demir.ab@protonmail.com>
2025-06-29 05:12:05 +0300

    Fix ability spelling for perfect mods

--- data/mods/perfectc1/pokedex.ts
+++ data/mods/perfectc1/pokedex.ts
@@ -3,91 +3,91 @@
 export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
 	weezing: {
 		inherit: true,
-		abilities: { 0: "Levitate", 1: "Neutralizing Gas", H: "Stench", E1: 'cloudnine' },
+		abilities: { 0: "Levitate", 1: "Neutralizing Gas", H: "Stench", E1: 'Cloud Nine' },
 	},
 	wigglytuff: {
 		inherit: true,
-		abilities: { 0: "Cute Charm", 1: "Competitive", H: "Frisk", E1: 'regenerator' },
+		abilities: { 0: "Cute Charm", 1: "Competitive", H: "Frisk", E1: 'Regenerator' },
 	},
 	silvally: {
 		inherit: true,
-		abilities: { 0: "RKS System", E1: 'cloudnine' },
+		abilities: { 0: "RKS System", E1: 'Cloud Nine' },
 	},
 	gogoat: {
 		inherit: true,
-		abilities: { 0: "Sap Sipper", H: "Grass Pelt", E1: 'grassysurge' },
+		abilities: { 0: "Sap Sipper", H: "Grass Pelt", E1: 'Grassy Surge' },
 	},
 	solrock: {
 		inherit: true,
-		abilities: { 0: "Levitate", E1: 'psychicsurge' },
+		abilities: { 0: "Levitate", E1: 'Psychic Surge' },
 	},
 	lickilicky: {
 		inherit: true,
-		abilities: { 0: "Own Tempo", 1: "Oblivious", H: "Cloud Nine", E1: 'mistysurge' },
+		abilities: { 0: "Own Tempo", 1: "Oblivious", H: "Cloud Nine", E1: 'Misty Surge' },
 	},
 	klinklang: {
 		inherit: true,
-		abilities: { 0: "Plus", 1: "Minus", H: "Clear Body", E1: 'electricsurge' },
+		abilities: { 0: "Plus", 1: "Minus", H: "Clear Body", E1: 'Electric Surge' },
 	},
 	// Steelix initially had 5 abilities, but one of them has been lost to time.
 	steelix: {
 		inherit: true,
-		abilities: { 0: "Rock Head", 1: "Sturdy", H: "Sheer Force", E1: 'rockypayload' },
+		abilities: { 0: "Rock Head", 1: "Sturdy", H: "Sheer Force", E1: 'Rocky Payload' },
 	},
 	jynx: {
 		inherit: true,
-		abilities: { 0: "Oblivious", 1: "Forewarn", H: "Dry Skin", E1: 'sapsipper' },
+		abilities: { 0: "Oblivious", 1: "Forewarn", H: "Dry Skin", E1: 'Sap Sipper' },
 	},
 	wailord: {
 		inherit: true,
-		abilities: { 0: "Water Veil", 1: "Oblivious", H: "Pressure", E1: 'unaware' },
+		abilities: { 0: "Water Veil", 1: "Oblivious", H: "Pressure", E1: 'Unaware' },
 	},
 	rotomfrost: {
 		inherit: true,
-		abilities: { 0: "Levitate", E1: 'refrigerate' },
+		abilities: { 0: "Levitate", E1: 'Refrigerate' },
 	},
 	magcargo: {
 		inherit: true,
-		abilities: { 0: "Magma Armor", 1: "Flame Body", H: "Weak Armor", E1: 'drought' },
+		abilities: { 0: "Magma Armor", 1: "Flame Body", H: "Weak Armor", E1: 'Drought' },
 	},
 	palossand: {
 		inherit: true,
-		abilities: { 0: "Water Compaction", H: "Sand Veil", E1: 'sandstream' },
+		abilities: { 0: "Water Compaction", H: "Sand Veil", E1: 'Sand Stream' },
 	},
 	relicanth: {
 		inherit: true,
-		abilities: { 0: "Swift Swim", 1: "Rock Head", H: "Sturdy", E1: 'sandrush' },
+		abilities: { 0: "Swift Swim", 1: "Rock Head", H: "Sturdy", E1: 'Sand Rush' },
 	},
 	lumineon: {
 		inherit: true,
-		abilities: { 0: "Swift Swim", 1: "Storm Drain", H: "Water Veil", E1: 'drizzle' },
+		abilities: { 0: "Swift Swim", 1: "Storm Drain", H: "Water Veil", E1: 'Drizzle' },
 	},
 	granbull: {
 		inherit: true,
-		abilities: { 0: "Intimidate", 1: "Quick Feet", H: "Rattled", E1: 'slushrush' },
+		abilities: { 0: "Intimidate", 1: "Quick Feet", H: "Rattled", E1: 'Slush Rush' },
 	},
 	delibird: {
 		inherit: true,
-		abilities: { 0: "Vital Spirit", 1: "Hustle", H: "Insomnia", E1: 'snowwarning' },
+		abilities: { 0: "Vital Spirit", 1: "Hustle", H: "Insomnia", E1: 'Snow Warning' },
 	},
 	haunter: {
 		inherit: true,
-		abilities: { 0: "Levitate", E1: 'flashfire' },
+		abilities: { 0: "Levitate", E1: 'Flash Fire' },
 	},
 	guzzlord: {
 		inherit: true,
-		abilities: { 0: "Beast Boost", E1: 'eartheater' },
+		abilities: { 0: "Beast Boost", E1: 'Earth Eater' },
 	},
 	mabosstiff: {
 		inherit: true,
-		abilities: { 0: "Intimidate", 1: "Guard Dog", H: "Stakeout", E1: 'strongjaw' },
+		abilities: { 0: "Intimidate", 1: "Guard Dog", H: "Stakeout", E1: 'Strong Jaw' },
 	},
 	toxicroak: {
 		inherit: true,
-		abilities: { 0: "Anticipation", 1: "Dry Skin", H: "Poison Touch", E1: 'lightningrod' },
+		abilities: { 0: "Anticipation", 1: "Dry Skin", H: "Poison Touch", E1: 'Lightning Rod' },
 	},
 	avalugg: {
 		inherit: true,
-		abilities: { 0: "Own Tempo", 1: "Ice Body", H: "Sturdy", E1: 'bulletproof' },
+		abilities: { 0: "Own Tempo", 1: "Ice Body", H: "Sturdy", E1: 'Bulletproof' },
 	},
 };
--- data/mods/perfectc2/pokedex.ts
+++ data/mods/perfectc2/pokedex.ts
@@ -3,115 +3,115 @@
 export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
 	klinklang: {
 		inherit: true,
-		abilities: { 0: "Plus", 1: "Minus", H: "Clear Body", E1: 'hadronengine' },
+		abilities: { 0: "Plus", 1: "Minus", H: "Clear Body", E1: 'Hadron Engine' },
 	},
 	aromatisse: {
 		inherit: true,
-		abilities: { 0: "Healer", H: "Aroma Veil", E1: 'mistysurge' },
+		abilities: { 0: "Healer", H: "Aroma Veil", E1: 'Misty Surge' },
 	},
 	jynx: {
 		inherit: true,
-		abilities: { 0: "Oblivious", 1: "Forewarn", H: "Dry Skin", E1: 'psychicsurge' },
+		abilities: { 0: "Oblivious", 1: "Forewarn", H: "Dry Skin", E1: 'Psychic Surge' },
 	},
 	gogoat: {
 		inherit: true,
-		abilities: { 0: "Sap Sipper", H: "Grass Pelt", E1: 'grassysurge' },
+		abilities: { 0: "Sap Sipper", H: "Grass Pelt", E1: 'Grassy Surge' },
 	},
 	hakamoo: {
 		num: 783,
 		inherit: true,
-		abilities: { 0: "Bulletproof", 1: "Soundproof", H: "Overcoat", E1: 'orichalcumpulse' },
+		abilities: { 0: "Bulletproof", 1: "Soundproof", H: "Overcoat", E1: 'Orichalcum Pulse' },
 	},
 	emolga: {
 		inherit: true,
-		abilities: { 0: "Static", H: "Motor Drive", E1: 'drizzle' },
+		abilities: { 0: "Static", H: "Motor Drive", E1: 'Drizzle' },
 	},
 	glalie: {
 		inherit: true,
-		abilities: { 0: "Inner Focus", 1: "Ice Body", H: "Moody", E1: 'snowwarning' },
+		abilities: { 0: "Inner Focus", 1: "Ice Body", H: "Moody", E1: 'Snow Warning' },
 	},
 	magcargo: {
 		inherit: true,
-		abilities: { 0: "Magma Armor", 1: "Flame Body", H: "Weak Armor", E1: 'sandstream' },
+		abilities: { 0: "Magma Armor", 1: "Flame Body", H: "Weak Armor", E1: 'Sand Stream' },
 	},
 	leavanny: {
 		inherit: true,
-		abilities: { 0: "Swarm", 1: "Chlorophyll", H: "Overcoat", E1: 'sapsipper' },
+		abilities: { 0: "Swarm", 1: "Chlorophyll", H: "Overcoat", E1: 'Sap Sipper' },
 	},
 	dhelmise: {
 		inherit: true,
-		abilities: { 0: "Steelworker", E1: 'waterabsorb' },
+		abilities: { 0: "Steelworker", E1: 'Water Absorb' },
 	},
 	exeggutor: {
 		inherit: true,
-		abilities: { 0: "Chlorophyll", H: "Harvest", E1: 'regenerator' },
+		abilities: { 0: "Chlorophyll", H: "Harvest", E1: 'Regenerator' },
 	},
 	raichualola: {
 		inherit: true,
-		abilities: { 0: "Surge Surfer", E1: 'swiftswim' },
+		abilities: { 0: "Surge Surfer", E1: 'Swift Swim' },
 	},
 	rotomfrost: {
 		inherit: true,
-		abilities: { 0: "Levitate", E1: 'solarpower' },
+		abilities: { 0: "Levitate", E1: 'Solar Power' },
 	},
 	carbink: {
 		inherit: true,
-		abilities: { 0: "Clear Body", H: "Sturdy", E1: 'icebody' },
+		abilities: { 0: "Clear Body", H: "Sturdy", E1: 'Ice Body' },
 	},
 	wigglytuff: {
 		inherit: true,
-		abilities: { 0: "Cute Charm", 1: "Competitive", H: "Frisk", E1: 'regenerator' },
+		abilities: { 0: "Cute Charm", 1: "Competitive", H: "Frisk", E1: 'Regenerator' },
 	},
 	lickilicky: {
 		inherit: true,
-		abilities: { 0: "Own Tempo", 1: "Oblivious", H: "Cloud Nine", E1: 'unaware' },
+		abilities: { 0: "Own Tempo", 1: "Oblivious", H: "Cloud Nine", E1: 'Unaware' },
 	},
 	stoutland: {
 		inherit: true,
-		abilities: { 0: "Intimidate", 1: "Sand Rush", H: "Scrappy", E1: 'slushrush' },
+		abilities: { 0: "Intimidate", 1: "Sand Rush", H: "Scrappy", E1: 'Slush Rush' },
 	},
 	camerupt: {
 		inherit: true,
-		abilities: { 0: "Magma Armor", 1: "Solid Rock", H: "Anger Point", E1: 'grasspelt' },
+		abilities: { 0: "Magma Armor", 1: "Solid Rock", H: "Anger Point", E1: 'Grass Pelt' },
 	},
 	steelix: {
 		inherit: true,
-		abilities: { 0: "Rock Head", 1: "Sturdy", H: "Sheer Force", E1: 'sandforce' },
+		abilities: { 0: "Rock Head", 1: "Sturdy", H: "Sheer Force", E1: 'Sand Force' },
 	},
 	palossand: {
 		inherit: true,
-		abilities: { 0: "Water Compaction", H: "Sand Veil", E1: 'raindish' },
+		abilities: { 0: "Water Compaction", H: "Sand Veil", E1: 'Rain Dish' },
 	},
 	relicanth: {
 		inherit: true,
-		abilities: { 0: "Swift Swim", 1: "Rock Head", H: "Sturdy", E1: 'sandrush' },
+		abilities: { 0: "Swift Swim", 1: "Rock Head", H: "Sturdy", E1: 'Sand Rush' },
 	},
 	wailord: {
 		inherit: true,
-		abilities: { 0: "Water Veil", 1: "Oblivious", H: "Pressure", E1: 'primordialsea' },
+		abilities: { 0: "Water Veil", 1: "Oblivious", H: "Pressure", E1: 'Primordial Sea' },
 	},
 	swanna: {
 		inherit: true,
-		abilities: { 0: "Keen Eye", 1: "Big Pecks", H: "Hydration", E1: 'solarpower' },
+		abilities: { 0: "Keen Eye", 1: "Big Pecks", H: "Hydration", E1: 'Solar Power' },
 	},
 	avalugg: {
 		inherit: true,
-		abilities: { 0: "Own Tempo", 1: "Ice Body", H: "Sturdy", E1: 'galvanize' },
+		abilities: { 0: "Own Tempo", 1: "Ice Body", H: "Sturdy", E1: 'Galvanize' },
 	},
 	haunter: {
 		inherit: true,
-		abilities: { 0: "Levitate", E1: 'flashfire' },
+		abilities: { 0: "Levitate", E1: 'Flash Fire' },
 	},
 	mabosstiff: {
 		inherit: true,
-		abilities: { 0: "Intimidate", 1: "Guard Dog", H: "Stakeout", E1: 'strongjaw' },
+		abilities: { 0: "Intimidate", 1: "Guard Dog", H: "Stakeout", E1: 'Strong Jaw' },
 	},
 	guzzlord: {
 		inherit: true,
-		abilities: { 0: "Beast Boost", E1: 'sandforce' },
+		abilities: { 0: "Beast Boost", E1: 'Sand Force' },
 	},
 	pinsir: {
 		inherit: true,
-		abilities: { 0: "Hyper Cutter", 1: "Mold Breaker", H: "Moxie", E1: 'protosynthesis' },
+		abilities: { 0: "Hyper Cutter", 1: "Mold Breaker", H: "Moxie", E1: 'Protosynthesis' },
 	},
 };

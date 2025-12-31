7153d6517bc0160622a416a4d244c8979855961e
demir <demir.ab@protonmail.com>
2026-01-01 00:48:06 +0300

    Add hint for Cryoresonance

--- data/mods/regenerations1/abilities.ts
+++ data/mods/regenerations1/abilities.ts
@@ -66,6 +66,8 @@ export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTa
 		onModifyType(move, pokemon) {
 			if (move.flags['sound'] && !pokemon.volatiles['dynamax']) { // hardcode
 				move.type = 'Ice';
+				this.add('-ability', pokemon, 'Cryoresonance');
+				this.hint(`Cryoresonance changed ${move.name} type to Ice.`);
 			}
 		},
 		flags: {},

bfc408b5e6a9e76e36f3b44e1f6976e3132185ac
demir <demir.ab@protonmail.com>
2026-01-20 06:17:04 +0300

    Add Gens Draft 4

---
+++ data/mods/generationsdraftgen4/pokedex.ts
@@ -0,0 +1,198 @@
+export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
+	clefablemega: {
+		inherit: true,
+		abilities: { 0: "Cute Charm" },
+	},
+	victreebelmega: {
+		inherit: true,
+		abilities: { 0: "Chlorophyll" },
+	},
+	raichumegax: {
+		inherit: true,
+		abilities: { 0: "Static" },
+	},
+	raichumegay: {
+		inherit: true,
+		abilities: { 0: "Static" },
+	},
+	starmiemega: {
+		inherit: true,
+		abilities: { 0: "Illuminate" },
+	},
+	dragonitemega: {
+		inherit: true,
+		abilities: { 0: "Inner Focus" },
+	},
+	meganiummega: {
+		inherit: true,
+		abilities: { 0: "Overgrow" },
+	},
+	feraligatrmega: {
+		inherit: true,
+		abilities: { 0: "Torrent" },
+	},
+	chimechomega: {
+		inherit: true,
+		abilities: { 0: "Levitate" },
+	},
+	absolmegaz: {
+		inherit: true,
+		abilities: { 0: "Pressure" },
+	},
+	skarmorymega: {
+		inherit: true,
+		abilities: { 0: "Keen Eye" },
+	},
+	staraptormega: {
+		inherit: true,
+		abilities: { 0: "Intimidate" },
+	},
+	froslassmega: {
+		inherit: true,
+		abilities: { 0: "Cursed Body" },
+	},
+	garchompmegaz: {
+		inherit: true,
+		abilities: { 0: "Rough Skin" },
+	},
+	lucariomegaz: {
+		inherit: true,
+		abilities: { 0: "Steadfast" },
+	},
+	heatranmega: {
+		inherit: true,
+		abilities: { 0: "Flash Fire" },
+	},
+	darkraimega: {
+		inherit: true,
+		abilities: { 0: "Bad Dreams" },
+	},
+	emboarmega: {
+		inherit: true,
+		abilities: { 0: "Blaze" },
+	},
+	excadrillmega: {
+		inherit: true,
+		abilities: { 0: "Sand Rush" },
+	},
+	golurkmega: {
+		inherit: true,
+		abilities: { 0: "Iron Fist" },
+	},
+	scolipedemega: {
+		inherit: true,
+		abilities: { 0: "Poison Point" },
+	},
+	scraftymega: {
+		inherit: true,
+		abilities: { 0: "Shed Skin" },
+	},
+	eelektrossmega: {
+		inherit: true,
+		abilities: { 0: "Levitate" },
+	},
+	chandeluremega: {
+		inherit: true,
+		abilities: { 0: "Flash Fire" },
+	},
+	chesnaughtmega: {
+		inherit: true,
+		abilities: { 0: "Overgrow" },
+	},
+	delphoxmega: {
+		inherit: true,
+		abilities: { 0: "Blaze" },
+	},
+	greninjamega: {
+		inherit: true,
+		abilities: { 0: "Torrent" },
+	},
+	meowsticmmega: {
+		inherit: true,
+		abilities: { 0: "Keen Eye" },
+	},
+	meowsticfmega: {
+		inherit: true,
+		abilities: { 0: "Keen Eye" },
+	},
+	pyroarmega: {
+		inherit: true,
+		abilities: { 0: "Rivalry" },
+	},
+	floettemega: {
+		inherit: true,
+		abilities: { 0: "Flower Veil" },
+	},
+	malamarmega: {
+		inherit: true,
+		abilities: { 0: "Contrary" },
+	},
+	barbaraclemega: {
+		inherit: true,
+		abilities: { 0: "Tough Claws" },
+	},
+	dragalgemega: {
+		inherit: true,
+		abilities: { 0: "Poison Point" },
+	},
+	hawluchamega: {
+		inherit: true,
+		abilities: { 0: "Limber" },
+	},
+	zygardemega: {
+		inherit: true,
+		abilities: { 0: "Aura Break" },
+	},
+	crabominablemega: {
+		inherit: true,
+		abilities: { 0: "Hyper Cutter" },
+	},
+	golisopodmega: {
+		inherit: true,
+		abilities: { 0: "Emergency Exit" },
+	},
+	drampamega: {
+		inherit: true,
+		abilities: { 0: "Berserk" },
+	},
+	magearnamega: {
+		inherit: true,
+		abilities: { 0: "Soul-Heart" },
+	},
+	magearnaoriginalmega: {
+		inherit: true,
+		abilities: { 0: "Soul-Heart" },
+	},
+	zeraoramega: {
+		inherit: true,
+		abilities: { 0: "Volt Absorb" },
+	},
+	falinksmega: {
+		inherit: true,
+		abilities: { 0: "Battle Armor" },
+	},
+	scovillainmega: {
+		inherit: true,
+		abilities: { 0: "Chlorophyll" },
+	},
+	glimmoramega: {
+		inherit: true,
+		abilities: { 0: "Toxic Debris" },
+	},
+	tatsugiricurlymega: {
+		inherit: true,
+		abilities: { 0: "Commander" },
+	},
+	tatsugiridroopymega: {
+		inherit: true,
+		abilities: { 0: "Commander" },
+	},
+	tatsugiristretchymega: {
+		inherit: true,
+		abilities: { 0: "Commander" },
+	},
+	baxcaliburmega: {
+		inherit: true,
+		abilities: { 0: "Thermal Exchange" },
+	},
+};

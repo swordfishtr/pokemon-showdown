7652837f15169d6072faab68ca97ab0122d8c6c2
demir <demir.ab@protonmail.com>
2026-01-21 03:11:24 +0300

    Gens Draft 4: Unban ZA Mega Stones

---
+++ data/mods/generationsdraftgen4/items.ts
@@ -0,0 +1,182 @@
+export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
+	absolitez: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	barbaracite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	baxcalibrite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	chandelurite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	chesnaughtite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	chimechite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	clefablite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	crabominite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	darkranite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	delphoxite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	dragalgite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	dragoninite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	drampanite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	eelektrossite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	emboarite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	excadrite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	falinksite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	feraligite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	floettite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	froslassite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	garchompitez: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	glimmoranite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	golisopite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	golurkite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	greninjite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	hawluchanite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	heatranite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	lucarionitez: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	magearnite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	malamarite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	meganiumite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	meowsticite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	pyroarite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	raichunitex: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	raichunitey: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	scolipite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	scovillainite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	scraftinite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	skarmorite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	staraptite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	starminite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	tatsugirinite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	victreebelite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	zeraorite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	zygardite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+};

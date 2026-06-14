e7d6fb7ed19fa74942e902682b2d949c1e4004e5
demir <demir.ab@protonmail.com>
2026-06-14 04:19:16 +0300

    fix d2

--- config/formats.ts
+++ config/formats.ts
@@ -1378,7 +1378,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		],
 		unbanlist: [
 			'Booster Energy', 'Noxious Torque',
-			'Tentacool', 'Sandygast', 'Finneon', 'Cubchoo', 'Larvesta', 'Skrelp', 'Jigglypuff', 'Golett', 'Togepi', 'Dreepy', 'Stunky', 'Gible', 'Varoom', 'Geodude-Alola', 'Sandile', 'Gimmighoul-Base', 'Dolliv', 'Litten', 'Magnemite', 'Croagunk', 'Smoochum', 'Skiploom', 'Pancham', 'Pineco', 'Natu', 'Corvisquire', 'Heatproof', 'Deino', 'Litleo', 'Marill', 'Munna', 'Shieldon',
+			'Parasect', 'Luvdisc', 'Delcatty', 'Tentacool', 'Delibird', 'Sandygast', 'Finneon', 'Cubchoo', 'Larvesta', 'Skrelp', 'Jigglypuff', 'Golett', 'Togepi', 'Dreepy', 'Stunky', 'Gible', 'Varoom', 'Geodude-Alola', 'Sandile', 'Gimmighoul-Base', 'Dolliv', 'Litten', 'Magnemite', 'Croagunk', 'Smoochum', 'Skiploom', 'Pancham', 'Pineco', 'Natu', 'Corvisquire', 'Heatproof', 'Deino', 'Litleo', 'Marill', 'Munna', 'Shieldon',
 		],
 		restricted: [
 			'Delcatty', 'Luvdisc', 'Parasect', 'Delibird',
